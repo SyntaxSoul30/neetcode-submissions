b7b174b0f06c1cece3a296c34a66e0a22824f00c
Siddhesh P Parab <82581407+SyntaxSoul30@users.noreply.github.com>
2026-06-26 06:08:01 -0500

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        var rows = matrix.length;
+        var columns = matrix[0].length;
+        for(int i = 0; i<rows; i++) {
+            for (int j=0; j<columns; j++) {
+                if(matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
